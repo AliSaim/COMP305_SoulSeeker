398c0fd719e3937176b4fa0663b40f04dea2333a
taerakwon <taerakwon@gmail.com>
2016-11-22 21:18:39 -0500

    Create Script folder and Game Controller class

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿/* Game Title: Soul Seeker
+ * Students: Taera Kwon (300755802), Ali Saim (300759480)
+ * Date Created: 2016-11-22
+ * Date Last Modified: 2016-11-22
+ * Revision History:
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
